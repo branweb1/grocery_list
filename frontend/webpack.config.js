fe7f8e673b2014c21542cb02eac0906721106a73
Brandon Webster <branweb@fastmail.com>
2023-08-07 18:16:02 -0700

    got frontend kinda working

---
+++ frontend/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 3000
+    hot: true,
+    liveReload: true
+  },
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+};
